3351c559e9920bb45bf8316b63b6bae80b8c0565
Pasan Wijekoon <112933799+PasanSWijekoon@users.noreply.github.com>
2026-01-15 02:04:45 +0530

    feat: add default metadata for Kosala Service Center

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { defaultMetadata } from '../utils/metadata';
 
-const SEO = ({ title, description }) => {
+const SEO = ({ title, description, keywords, ogImage, ogUrl }) => {
+  const metaTitle = title ? `Kosala Service Center | ${title}` : defaultMetadata.title;
+  const metaDescription = description || defaultMetadata.description;
+  const metaKeywords = keywords || defaultMetadata.keywords;
+  const metaImage = ogImage || defaultMetadata.ogImage;
+  const metaUrl = ogUrl || defaultMetadata.ogUrl;
+
   return (
     <Helmet>
-      <title>Kosala Service Center | {title}</title>
-      <meta name="description" content={description} />
+      {/* Standard Metadata */}
+      <title>{metaTitle}</title>
+      <meta name="description" content={metaDescription} />
+      <meta name="keywords" content={metaKeywords} />
+      <meta name="author" content={defaultMetadata.author} />
+
+      {/* Open Graph Metadata */}
+      <meta property="og:title" content={metaTitle} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:image" content={metaImage} />
+      <meta property="og:url" content={metaUrl} />
+      <meta property="og:type" content={defaultMetadata.ogType} />
+      <meta property="og:site_name" content={defaultMetadata.siteName} />
+
+      {/* Twitter Card Metadata */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={metaTitle} />
+      <meta name="twitter:description" content={metaDescription} />
+      <meta name="twitter:image" content={metaImage} />
     </Helmet>
   );
 };
---
+++ src/utils/metadata.js
@@ -0,0 +1,10 @@
+export const defaultMetadata = {
+  title: 'Kosala Service Center | Premium Car Wash & Vehicle Maintenance in Anuradhapura',
+  description: 'Kosala Service Center offers premium car detailing, body wash, interior cleaning, undercarriage wash, and vehicle repairs in Anuradhapura. Best service for your vehicle.',
+  keywords: 'car wash, vehicle maintenance, car detailing, interior cleaning, undercarriage wash, vehicle repair, Anuradhapura, auto care',
+  author: 'Kosala Service Center',
+  ogImage: '/images/car-shop-logo-V49ANM.png',
+  ogType: 'website',
+  ogUrl: 'https://kosalaservicecenter.com/',
+  siteName: 'Kosala Service Center',
+};
